ec447f46771a044161311067a161bbad44a41fe6
Hitusss <kuba.niespielak@gmail.com>
2026-01-29 13:24:02 +0100

    feat(tools/eslint): setup base config

---
+++ tools/eslint/base.ts
@@ -0,0 +1,26 @@
+import antfu from "@antfu/eslint-config";
+
+export default function config({options, config}: {options: Parameters<typeof antfu>[0] , config: Parameters<typeof antfu>[1] }): any {
+  return antfu(
+    {
+      type: "app",
+      typescript: true,
+      react: true,
+      formatters: false,
+      stylistic: false,
+      ...options,
+    },
+    {
+      rules: {
+        "unicorn/filename-case": [
+          "error",
+          {
+            case: "kebabCase",
+            ignore: ["README.md"],
+          },
+        ],
+      },
+      ...config,
+    },
+  );
+}
